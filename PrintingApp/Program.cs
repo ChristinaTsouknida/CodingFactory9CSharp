bf7fd0bfd77525dd58c18ea36137eb4507dbbb7d
ChristinaTsouknida <christina.tsouknida@gmail.com>
2026-03-09 19:35:34 +0200

    chore: add printing logic

---
+++ PrintingApp/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace PrintingApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 0;
+
+
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Num = {0}", num);
+            Console.WriteLine($"Num = {num}");
+        }
+    }
+}
