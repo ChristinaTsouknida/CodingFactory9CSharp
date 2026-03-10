6fec7c118ffad1b9f1d6b0c6fa258ab51aa704ca
ChristinaTsouknida <christina.tsouknida@gmail.com>
2026-03-10 19:21:14 +0200

    chore: add do while

---
+++ DoWhileApp/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace DoWhileApp
+{
+
+    /// <summary>
+    /// Counts the number of digits of an int
+    /// </summary>
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 0;
+            int numberOfDigits = 0;
+            int tmp = 0;
+
+            Console.WriteLine("Please enter an integer");
+
+            if (!int.TryParse(Console.ReadLine(), out num))
+            {
+                Console.WriteLine("Invalid input for integer.");
+                return;
+            }
+
+            tmp = num;
+
+            do
+            {
+                tmp /= 10;
+                numberOfDigits++;
+
+            } while (tmp != 0);
+
+            Console.WriteLine($"Digits count: {numberOfDigits}");
+        }
+            
+    }
+}
