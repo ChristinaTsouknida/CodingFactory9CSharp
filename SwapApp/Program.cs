d6db8fe5189a5c61443f6d958bddfe0d221f20d7
ChristinaTsouknida <christina.tsouknida@gmail.com>
2026-03-10 20:15:51 +0200

    chore: add swap logic, also with ref and tuples

---
+++ SwapApp/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace SwapApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1;
+            int b = 2;
+            Swap(a, b);
+
+            Console.WriteLine($"a: {a}, b: {b}");
+        }
+
+
+
+        /// <summary>
+        /// Pass by value swap method. It does not swap the values of a and b in the caller method.
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        public static void Swap(int a, int b)
+        {
+            int tmp = a;
+            a = b;
+            b = tmp;
+        }
+
+
+        public static void SwapRef(ref int a, ref int b)
+        {
+            int tmp = a;
+            a = b;
+            b = tmp;
+
+        }
+
+
+        public static (int, int) SwapWithTuple(int a, int b)
+        {
+            return (b, a);
+
+        }
+    }
+}
