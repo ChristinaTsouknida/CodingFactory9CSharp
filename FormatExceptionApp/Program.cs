0ef7811755a5a2356a2456e7a6e29a108a7d5c4e
ChristinaTsouknida <christina.tsouknida@gmail.com>
2026-03-10 19:09:03 +0200

    feat: add FormatException example

---
+++ FormatExceptionApp/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace FormatExceptionApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 0;
+
+            while (true) 
+            {
+                try
+                {
+                    Console.WriteLine("Enter an integer (0 for Quit)");
+                    num = int.Parse(Console.ReadLine()!);
+                    Console.WriteLine($"You entered the integer: {num}");
+
+                    if (num == 0) break;
+                }
+                catch (FormatException e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+
+
+            }
+        }
+    }
+}
