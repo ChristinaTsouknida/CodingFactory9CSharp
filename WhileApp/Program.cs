545615d7f65728ead87791854ffb1c6a3589a573
ChristinaTsouknida <christina.tsouknida@gmail.com>
2026-03-10 19:09:53 +0200

    chore: add switch examples with classic switch case and switch expression and switch pattern

---
+++ WhileApp/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace WhileApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const int END = 10;
+
+            int sum = 0;
+            int i = 1;
+
+            while (i <= END)      // 10 iterations
+            {
+                sum += i;
+                i++;
+            }
+
+            Console.WriteLine($"The sum from 1 to {END} is: {sum}");
+        }
+    }
+}
