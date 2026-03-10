3df375fd9ddb5fd85d2a46d551bb89b986b250bc
ChristinaTsouknida <christina.tsouknida@gmail.com>
2026-03-10 18:26:25 +0200

    chore: add read logic with tryparse

---
+++ NumberRead/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace NumberRead
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float floatNum = 0F;
+            double doubleNum = 0D;
+
+            Console.WriteLine("Εισάγετε δύο δεκαδικούς");
+
+            if (!float.TryParse(Console.ReadLine(), out floatNum))
+            {
+                Console.WriteLine("Μη έγκυρη είσοδος για float.");
+                return;
+            }
+
+            if (!double.TryParse(Console.ReadLine(), out doubleNum))
+            {
+                Console.WriteLine("Μη έγκυρη τιμή για double");
+                return;
+            }
+
+            Console.WriteLine($"float = {floatNum,-10:N2}\t double = {doubleNum, -10:N2}");
+        }
+    }
+}
