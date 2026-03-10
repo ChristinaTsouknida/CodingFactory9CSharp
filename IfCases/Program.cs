48d9eaa8cf901b2a9df636a07da85caaba3488a4
ChristinaTsouknida <christina.tsouknida@gmail.com>
2026-03-10 19:10:50 +0200

    chore: add if logic

---
+++ IfCases/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace IfCases
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 20;
+            string name = null;
+            string nickname = "Ally";
+
+            if (age > 18)
+            {
+                Console.WriteLine("Adult");
+            }
+            else
+            {
+                Console.WriteLine("Minor");
+            }
+
+            var status = age > 18 ? "Adult" : "Minor";
+            Console.WriteLine(status);   //Adult
+
+            var displayName = name ?? "Unknown";
+            Console.WriteLine(displayName);
+
+            var length = name?.Length ?? 0;
+            Console.WriteLine(length);
+        }
+    }
+}
