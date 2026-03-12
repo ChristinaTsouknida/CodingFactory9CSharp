62a557d0477da7569eaf01a7c781491641dd3b4a
ChristinaTsouknida <christina.tsouknida@gmail.com>
2026-03-12 20:23:56 +0200

    chore: add POCO (Plain Old CLR Class) classes with public properties, constructors and equals, GetHashCode and ToString

---
+++ PocoApp/Course.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PocoApp
+{
+    /// <summary>
+    /// auto implemented properties
+    /// CLR creates private backing fields automatically
+    /// </summary>
+    internal class Course
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+
+        public override string ToString() =>        
+           $"Course: Id={Id}, Title={Title}, Description={Description}";
+       
+
+        public override bool Equals(object? obj)
+        {
+            return obj is Course course &&
+                   Id == course.Id &&
+                   Title == course.Title &&
+                   Description == course.Description;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Id, Title, Description);
+        }
+    }
+}
---
+++ PocoApp/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace PocoApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student alice = new Student();
+            Student bob = new();
+            var julia = new Student();
+
+
+            Teacher teacher = new (1, "Μάκης", "Καπέτης");
+            Teacher teacher2 = new() { Id = 2, Firstname = "Γιάννης", Lastname = "Παπαδόπουλος" };
+
+
+
+
+        }
+    }
+}
