44d9809e8bdd0cf067d1340d62bfabfb6463da7e
ChristinaTsouknida <christina.tsouknida@gmail.com>
2026-03-12 20:51:08 +0200

    chore: add exceptions logic with a standard pattern

---
+++ AccountApp/Exceptions/InsufficientBalanceException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AccountApp.Exceptions
+{
+    internal class InsufficientBalanceException : Exception
+    {
+        public InsufficientBalanceException()
+            : base("Insufficient balance")
+        {  }
+
+
+            public InsufficientBalanceException(string message)
+                : base(message)
+            {  }
+    
+            public InsufficientBalanceException(string message, Exception innerException)
+                : base(message, innerException)
+            { }
+
+    }
+}
---
+++ AccountApp/Exceptions/InvalidssnException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AccountApp.Exceptions
+{
+    internal class InvalidssnException : Exception
+    {
+        public InvalidssnException()
+         : base("The provided SSN is invalid")
+        { }
+
+        public InvalidssnException(string message)
+            : base(message)
+        { }
+
+        public InvalidssnException(string message, Exception innerException)
+            : base(message, innerException)
+        { }
+    }
+}
