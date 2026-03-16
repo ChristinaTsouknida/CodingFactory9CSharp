a17bef13125648adce0e86e19420cb9b3b393416
ChristinaTsouknida <christina.tsouknida@gmail.com>
2026-03-16 20:18:16 +0200

    chore: add interfaces and skeletal implementation logic

---
+++ InterfacesApp/Point.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InterfacesApp
+{
+    internal class Point : AbstractPoint, IMoveable
+    {
+
+        public override string ToString() => $"X={X}";
+
+    }
+}
