778b4b9c6328feb30208950ef9941c35c25748ea
ChristinaTsouknida <christina.tsouknida@gmail.com>
2026-03-16 20:18:00 +0200

    chore: add abstract class logic

---
+++ AbstractApp/AbstractAnimal.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AbstractApp
+{
+    internal abstract class AbstractAnimal
+    {
+
+        public int Id { get; set; }
+
+        public string? Name { get; set; }
+
+        public double Age { get; set; }
+
+
+        public abstract override string ToString();
+
+
+        public virtual void Eat()
+        {
+            Console.WriteLine($"{Name} is eating.");
+        }
+    }
+}
