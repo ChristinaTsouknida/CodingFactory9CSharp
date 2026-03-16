626ffd33fcf6564d823636ca6364b61270cb85e6
ChristinaTsouknida <christina.tsouknida@gmail.com>
2026-03-16 19:48:13 +0200

    chore: add sorting logic with sort, IComparable

---
+++ CollectionsSort/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace CollectionsSort
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Student> students =
+                [
+                    new Student() {Firstname = "Alice", Lastname = "Wonderland", Age = 22},
+                    new Student() {Firstname = "Bob", Lastname = "Dylan", Age = 20},
+
+                ];
+
+            students.Sort(); // uses compareTo
+            students.Sort((s1, s2) => string.Compare(s1.Lastname, s2.Lastname, StringComparison.Ordinal));   // IComparer
+
+        }
+    }
+}
