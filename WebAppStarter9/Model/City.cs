fab344c30119f397e245e63a394ca15938a5e3a3
ChristinaTsouknida <christina.tsouknida@gmail.com>
2026-03-18 19:30:35 +0200

    chore: add model city

---
+++ WebAppStarter9/Model/City.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages.Infrastructure;
+
+namespace WebAppStarter9.Model
+{
+    public class City
+    {
+        public int Id { get; set; }
+
+        public string? Name { get; set; }
+    }
+}
